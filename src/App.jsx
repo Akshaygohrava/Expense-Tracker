613ce60034cf52be6ec6b0e49d1e5067c26191e8
Akshay Gohrava <akshaygohrava@gmail.com>
2025-05-25 00:09:26 +0530

    Expanse Tracker Application

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello, This is Akshay Gohrava</h1>
+      <p>Building The Spa like expense tracker</p>
+    
+    </>
+  )
+}
+
+export default App
