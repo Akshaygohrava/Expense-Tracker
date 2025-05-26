fe2220c7c5f02c6f8200fb4ba102771a41e3634c
Akshay Gohrava <akshaygohrava@gmail.com>
2025-05-26 13:22:47 +0530

    Expanse Tracker Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+// import { Routes, Route } from 'react-router-dom'
+// import Navbar from './components/Navbar'
+// import Dashboard from './pages/Dashboard'
+// import AddExpense from './pages/AddExpense'
+// import Login from './pages/Login'
+import './index.css'
 import './App.css'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Hello, This is Akshay Gohrava</h1>
+      <h1 className="text-2xl font-bold text-blue-600">Hello Tailwind!</h1>
+
       <p>Building The Spa like expense tracker</p>
     
     </>
